f918395b7d5d0b1e8e6df303f15286f029347e32
Nathaniel Boucher <nathanielboucher@gmail.com>
2020-01-30 22:57:50 -0800

    Minor style changes to sample project; Added constants;

--- samples/PutMoviesInFolders/Program.cs
+++ samples/PutMoviesInFolders/Program.cs
@@ -8,32 +8,45 @@ using Fluent.IO;
 
 namespace PutMoviesInFolders {
     class Program {
-        static void Main(string[] args) {
+        private const string HELP_TEXT = 
+            @"Copies all files in the directory into their own folder.
+            Usage:
+            putmoviesinfolders [path]
+            where [path] is the path of the folder to process.";
+
+        static void Main(string[] args)
+        {
+            PrintHelpSection(args);
+
+            Path.Get(args.Length != 0 ? args[0] : ".")
+                .Files(p => new[] {
+                            ".avi",
+                            ".m4v",
+                            ".wmv",
+                            ".mp4",
+                            ".dvr-ms",
+                            ".mpg",
+                            ".mkv"
+                        }
+                        .Contains(p.Extension))
+                .CreateDirectories(p => p.Parent()
+                                        .Combine(p.Extension))
+                .End()
+                .Move(p => p.Parent()
+                          .Combine(p.Extension)
+                          .Combine(p.FileName));
+        }
+
+        private static void PrintHelpSection(string[] args)
+        {
             if (args.Length == 1 && (
                 args[0] == "help" ||
                 args[0] == "/?" ||
-                args[0] == "-?")) {
-                Console.WriteLine(@"Copies all files in the directory into their own folder.
-
-Usage:
-putmoviesinfolders [path]
-where [path] is the path of the folder to process.");
+                args[0] == "-?"))
+            {
+                Console.WriteLine(HELP_TEXT);
                 return;
             }
-            Path.Get(args.Length != 0 ? args[0] : ".")
-                .Files(
-                    p => new[] {
-                        ".avi", ".m4v", ".wmv",
-                        ".mp4", ".dvr-ms", ".mpg", ".mkv"
-                    }.Contains(p.Extension))
-                .CreateDirectories(
-                    p => p.Parent()
-                          .Combine(p.FileNameWithoutExtension))
-                .End()
-                .Move(
-                    p => p.Parent()
-                          .Combine(p.FileNameWithoutExtension)
-                          .Combine(p.FileName));
         }
     }
 }
